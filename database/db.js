0eb18a0b43776bb46b8583944ae847c795b20ff7
Mohamed Sarhan <mohsarhan88@gmail.com>
2023-05-27 19:44:43 +0300

    edit db message

--- database/db.js
+++ database/db.js
@@ -4,7 +4,7 @@ const DB_URL = require('../config/config').DB_URL;
 async function db() {
     try {
         await mongoose.connect(DB_URL);
-        console.log(`DB Connectioned: ${DB_URL}`);
+        console.log(`DB connection: ${DB_URL}`);
     } catch (error) {
         console.log("ERROR in DB connection: ", error)
 
